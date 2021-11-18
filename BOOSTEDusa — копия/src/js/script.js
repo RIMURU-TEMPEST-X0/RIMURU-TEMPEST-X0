6c25892ee6194577620719ff5db8f415a14e206e
Reymuru-Tempest <92702960+RIMURU-TEMPEST-X0@users.noreply.github.com>
2021-11-18 15:54:58 +0300

    BOOSTED-USA

---
+++ BOOSTEDusa — копия/src/js/script.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+    $(".carousel").owlCarousel({
+        items: 7,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplaySpeed: 1000,
+        autoplayHoverPause: true,
+        dots: true,
+        touchDrag: true,
+    });
+});
